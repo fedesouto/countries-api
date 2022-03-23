5e7e03bdea3a6d1e584ceb83aaf8f6dcde5aa812
fedesouto <federicogsouto@gmail.com>
2022-03-23 15:56:54 -0300

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import CountryListContainer from "./components/CountryListContainer";
 import Header from "./components/Header";
 
@@ -6,7 +7,10 @@ const App = () => {
   return (
     <div>
       <Header />
-      <CountryListContainer />
+      <BrowserRouter>
+        <Route exact path="/" element={<CountryListContainer />}/>
+        <Route exact path="/:region" element={<CountryListContainer />}/>
+      </BrowserRouter>
     </div>
   );
 };
